dad90b2c2754c8ed1e7618fe7178d0c03dd66fd5
BlueForestIcarus <erichspaker@gmail.com>
2016-02-08 04:40:03 +0000

    finished code to find the target in aquire.py

--- acquire.py
+++ acquire.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python
 
-from pylab import plot, imshow as show, ion
+from pylab import plot, imshow as show, ion, axvline as vline, axhline as hline
 import cPickle as pickle, numpy as np
+
+WIDTH = 320
+HEIGHT = 240
 
 ion()
 
@@ -10,11 +13,8 @@ with open("diff.np", "r") as f:
 
 print("code start")
 
-mono_diff = np.sum(diff, axis=2)
+mono_diff = np.sum(diff, axis=2) 
 vsum = np.sum(mono_diff, axis=0)
-hsum = np.sum(mono_diff, axis=1)
-
-#hv = [v for v in range(len(vsum)) if vsum[v] > not_median]
 
 #constant to filter peaks in vsum (middle of range)
 threshold = (np.max(vsum) - np.min(vsum)) / 2 
@@ -39,14 +39,60 @@ if len(segments) == 2:
 	p0 = (segments[0][0] + segments[0][1])/2 #middle of first hill
 	p1 = (segments[1][0] + segments[1][1])/2 #middle of second hill
 
-    #distance from the center of the image tot the center of the target, in pixels
-	offset = (p0 + p1)/2 - 180 
+	#distance from the center of the image tot the center of the target, in pixels
+	offsetX = (p0 + p1)/2 - WIDTH/2
    	
-    #gap in pixels between the center of two hills
-    gap = abs(p0 - p1)
+	#gap in pixels between the center of two hills
+	gap = abs(p0 - p1)
+ 
+	hsum = np.sum(mono_diff[..., segments[0][0] : segments[1][1]], axis=1) #horizontal sum
+	
+   
+	state = 0 #state of the hsum scanner
+	thresholds = (2000, 10000) #thresholds 
+	points = [] #[v-bars, h-bar, bottom]
 
-	print(str(offset) + " " + str(gap))
+	for i, v in enumerate(hsum):
+		#above target
+		if state == 0:
+			if v > thresholds[0]:
+				state = 1
+				points.append(i)
+
+
+		#vertical bars
+		elif state == 1:
+			if v > thresholds[1]:
+				state = 2
+				points.append(i)
+
+			elif v < thresholds[0]:
+				state = 0
+				points.pop()
+
+		#horizontal bar
+		elif state == 2:
+			if v < thresholds[0]:           
+				points.append(i)
+				break
+
+	print("points: " + str(points))  
+
+	if len(points) == 3:
+		#distance from the bottom of the view to the bottom of the target
+		offsetY = HEIGHT - (points[2] + points[1])/2
+		
+		print("offsetX: %d offsetY: %d" % (offsetX, offsetY))
+		show(diff)
+		vline(offsetX + WIDTH/2)
+		hline(HEIGHT - offsetY)
+		
+	else:
+		print("ERROR: " + str(len(points)) + " hsum points instead of 3?")
+		
 
 else:
-	print("ERROR: found " + str(len(segments)) + " segments. Expected 2.")
+	print("ERROR: found " + str(len(segments)) + " vsum segments (vertical bars). Expected 2.")
 	
+
+
