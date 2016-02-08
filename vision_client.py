f1559d34897175db560ec53d6f22c65e647962aa
BlueForestIcarus <erichspaker@gmail.com>
2016-02-08 07:24:30 +0000

    some work on the client to run on the roborio

--- vision_client.py
+++ vision_client.py
@@ -1,6 +1,7 @@
 #! /usr/bin/python
 
 import socket, struct
+import os
 from time import time, sleep
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
@@ -9,7 +10,9 @@ s.settimeout(10)
 
 message = '192.168.7.1'
 
-while True:
+tmp= open("/tmp/memes.dat", "w+")
+
+while False:
 	start_time = (time() * 1000.0)
 	s.sendto(message, ('192.168.7.2', 8474))
 	rx = s.recv(1024)
@@ -17,3 +20,9 @@ while True:
 	loop_time = (time() * 1000.0) - start_time
 	print "Received message from beaglebone, with %sms round trip." % loop_time
 	sleep(0.5)
+
+def write(text):
+   tmp.truncate()
+   tmp.write(text)
+   tmp.flush()
+
