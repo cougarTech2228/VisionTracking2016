85b7649b1493b79d52915c181b0ceb9b9f5f165a
Python Drew <debian@beaglebone.localdomain>
2016-02-10 02:39:17 +0000

    intermediate version probably has errors..

--- xenovid.py
+++ xenovid.py
@@ -1,12 +1,13 @@
+
 import os, sys
 sys.path.append(os.getcwd())
 import numpy as np
 import _ct
 a,b,vid_conf_addr = _ct.addrs()
 from ctypes import c_ubyte
-from stronghold import WIDTH, HEIGHT, vidconf_s
 import copy
 
+from stronghold import WIDTH, HEIGHT, vidconf_s
 vc = vidconf_s.from_address(vid_conf_addr)
 
 Image=(((c_ubyte*3)*WIDTH)*HEIGHT)
@@ -23,36 +24,46 @@ ion()
 from cv2 import cv
 from time import time
 
-class DisplayThread(threading.Thread):
+class VideoThread(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
         self.daemon = True
+        self.watch_keys = False
+        self.showsig = False
+        self.showback = False
+        self.showdiff = False
+        self.verbose = False
+
     def run(self):
         while True :
-            #cv2.imshow("sig", sig)
-            #cv2.imshow("back", back)
-            
-            self.diff = cv2.subtract(sig, back)
-            #cv2.imshow("diff", self.diff) 
+            if self.showsig :
+                cv2.imshow("sig", sig)
+            if self.showback :
+                cv2.imshow("back", back)
+            if self.showdiff :
+                cv2.imshow("diff", self.diff) 
             
             self.process()            
 
-            key = cv2.waitKey(20)
-            if key == -1:
-                continue
-            if key == ord('s') or key == ord('S'):
-                with open("diff.np", "w") as f:
-                    pickle.dump(self.diff, f)
-                with open("sig.np", "w") as f:
-                    pickle.dump(sig, f)
-                with open("back.np", "w") as f:
-                    pickle.dump(back, f)
-
-                continue
+            if self.watch_keys :
+                key = cv2.waitKey(20)
+                if key == -1:
+                    continue
+                if key == ord('s') or key == ord('S'):
+                    with open("diff.np", "w") as f:
+                        pickle.dump(self.diff, f)
+                    with open("sig.np", "w") as f:
+                        pickle.dump(sig, f)
+                    with open("back.np", "w") as f:
+                        pickle.dump(back, f)
+                    continue
+            else :
+                cv2.waitKey(1) 
 
     def process(self):
         global offsetX, offsetY
     
+        self.diff = cv2.subtract(sig, back) # could do the diff in C code.
         bdiff, gdiff, rdiff = cv2.split(self.diff)
         
         mono_diff = cv2.subtract(gdiff, rdiff)
@@ -70,29 +81,51 @@ class DisplayThread(threading.Thread):
         segments = [] #peaks collapsed to line segments
     
         #scans through vsum to find peaks
-        for i, v in enumerate(vsum):
-            if search:
-                if v>threshold:
-                    search=False
-                    tmp_point = i
-            else:
-                if v<threshold:
-                    search= True
-                    segments.append( (tmp_point,i) )
+        colsums = enumerate(vsum)
 
-        if len(segments) == 2:
-            p0 = (segments[0][0] + segments[0][1])/2 #middle of first hill
-            p1 = (segments[1][0] + segments[1][1])/2 #middle of second hill
+        for col, csum in colsums :
+            if csum > threshold : # we found a column over threshold
+                peaksum = csum
+                peakstart = col
+                for col, csum in colsums : # continue iterating over the columns...
+                    if csum > threshold :
+                        peaksum += csum
+                    else : # we have fallen below threshold again. peak has been found.
+                        peakend = col - 1
+                        # each segment is the TOTAL sum in the peak, plus the start and end pixel
+                        segments.append((peaksum, (peakstart, peakend)))
+                        break
+                
+        # we will VERY often get more than 2.. camera might see 6!!
+        # find two peaks next to each other of sensible separation and strength.
+        # we want the two strongest ones.
+        if len(segments) > 2 :  # if we DO get more than 2
+            sums = [seg[0] for seg in segments] # just look at their strengths.
+            strongest = max(sums) 
+            if sums[0] == strongest :
+                segments = segments[:2]
+            elif sums[-1] == strongest
+                segments = segments[-2:]
+            else :
+                ix = sums.index(strongest)
+                if sums[ix + 1] > sums[ix - 1] :
+                    segments = segments[ix : ix + 2]
+                else :
+                    segments = segments[ix - 1 : ix + 1]
+        # actually, we pick the strongest peak
+        # and its strongest neighbor.
+        if len(segments) == 2 :
+            peaks = [sum(s[1])/2 for s in segments]
 
             #distance from the center of the image tot the center of the target, in pixels
-            offsetX = (p0 + p1)/2 - WIDTH/2
+            offsetX = sum(peaks)/2 - WIDTH/2
         
             #gap in pixels between the center of two hills
-            gap = abs(p0 - p1)
+            gap = peaks[1] - peaks[0]
+            # We can do a sanity check on the gap, too.
      
-            hsum = np.sum(mono_diff[..., segments[0][0] : segments[1][1]], axis=1) #horizontal sum
+            hsum = np.sum(mono_diff[..., peaks[0] : peaks[1]], axis=1) #horizontal sum
         
-       
             state = 0 #state of the hsum scanner
             hmax = np.max(hsum)
             thresholds = (hmax/10, hmax/2) #thresholds 
@@ -126,21 +159,18 @@ class DisplayThread(threading.Thread):
                 #distance from the bottom of the view to the bottom of the target
                 offsetY = HEIGHT - (points[2] + points[1])/2
                 self.display(True)
-                        
             else:
-                print("ERROR: " + str(len(points)) + " hsum points instead of 3?")
+                if self.verbose :
+                    print("ERROR: " + str(len(points)) + " hsum points instead of 3?")
                 self.display(False)
-                pass
-            
-
         else:
-            print("ERROR: found " + str(len(segments)) + " vsum segments (vertical bars). Expected 2.")
+            if self.verbose :
+                print("ERROR: found " + str(len(segments)) + " vsum segments (vertical bars). Expected 2.")
             self.display(False)
-            pass
 
         
     def display(self, found):
-        disp_img = copy.copy(sig)
+        disp_img = sig.copy()
     
         if(found):
             cv2.line(disp_img,(WIDTH/2 + offsetX,0),(WIDTH/2 + offsetX,HEIGHT),(0,0,255),3)
@@ -150,8 +180,8 @@ class DisplayThread(threading.Thread):
     
         cv2.imshow("display",disp_img)
 
-dt = DisplayThread()
-dt.start()
+vt = VideoThread()
+vt.start()
 
 import socket
 from time import sleep
