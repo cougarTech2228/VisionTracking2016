2a242452dca948639720620a232c604aed3b8205
Fred Frey <f.frey6@gmail.com>
2016-02-13 02:51:49 +0000

    publishing to NetworkTables

--- xenovid.py
+++ xenovid.py
@@ -23,6 +23,8 @@ from pylab import plot, ion
 ion()
 from cv2 import cv
 from time import time
+from networktables import NetworkTable
+sd = NetworkTable.getTable("SmartDashboard")
 
 class VideoThread(threading.Thread):
     def __init__(self):
@@ -39,6 +41,7 @@ class VideoThread(threading.Thread):
         self.verbose = False
         self.testmode = False
         self.showfound = True
+        self.minthresh = 1000
 
     def run(self):
         while True :
@@ -77,16 +80,14 @@ class VideoThread(threading.Thread):
             self.diff = sig # np.array(sig, dtype="int16") # test with white on black target
         else :
             self.diff = cv2.subtract(sig, back) # could do the diff in C code.
-        # next method is much faster (if it works)
-        if True : 
-            bdiff, gdiff, rdiff = [self.diff[:,:,i] for i in range(3)] #lightweight
-        else :
-            bdiff, gdiff, rdiff = cv2.split(self.diff) # not sure what split does internally
+
+        bdiff, gdiff, rdiff = [self.diff[:,:,i] for i in range(3)] #lightweight
         
         if self.testmode :
             self.mono = mono_diff = bdiff/3 + gdiff/3 + rdiff/3
         else :
             mono_diff = cv2.subtract(gdiff, rdiff)
+
         vsum = np.sum(mono_diff, axis=0)
         
         if self.showr :
@@ -99,7 +100,9 @@ class VideoThread(threading.Thread):
             cv2.imshow("mono", mono_diff)
     
         #constant to filter peaks in vsum (middle of range)
-        threshold = (np.max(vsum) + np.min(vsum)) / 2 
+        self.threshold = (np.max(vsum) + np.min(vsum)) / 2 
+        if self.threshold < self.minthresh :
+            self.threshold = self.minthresh
     
         search = True #is loop if looking for a peak
         segments = [] #peaks collapsed to line segments
@@ -108,11 +111,11 @@ class VideoThread(threading.Thread):
         colsums = enumerate(vsum)
 
         for col, csum in colsums :
-            if csum > threshold : # we found a column over threshold
+            if csum > self.threshold : # we found a column over threshold
                 peaksum = csum
                 peakstart = col
                 for col, csum in colsums : # continue iterating over the columns...
-                    if csum > threshold :
+                    if csum > self.threshold :
                         peaksum += csum
                     else : # we have fallen below threshold again. peak has been found.
                         peakend = col - 1
@@ -170,37 +173,21 @@ class VideoThread(threading.Thread):
                             end = row
                             self.offsetY = ((start + end)-HEIGHT)/2
                             self.display(True)
+                            if self.verbose :
+                                print("found x y", self.offsetX, self.offsetY )
+                            sd.putNumber('boneX', self.offsetX)
+                            sd.putNumber('boneY', self.offsetY)
+                            sd.putNumber('boneFound', 1)
                             return
             self.display(False)
-            """
-                elif state == 1:
-                    if v > thresholds[1]:
-                        state = 2
-                        points.append(i)
-
-                    elif v < thresholds[0]:
-                        state = 0
-                        points.pop()
-
-                #horizontal bar
-                elif state == 2:
-                    if v < thresholds[0]:           
-                        points.append(i)
-                        break
-
-            if len(points) == 3:
-                #distance from the bottom of the view to the bottom of the target
-                offsetY = HEIGHT - (points[2] + points[1])/2
-                self.display(True)
-            else:
-                if self.verbose :
-                    print("ERROR: " + str(len(points)) + " hsum points instead of 3?")
-                self.display(False)
-            """
-        else:
             if self.verbose :
-                print("ERROR: found " + str(len(segments)) + " vsum segments (vertical bars). Expected 2.")
+                print("missed horizontal bar! x", self.offsetX)
+                sd.putNumber('boneX', self.offsetX)
+        else:
             self.display(False)
+            sd.putNumber('boneFound', 1)
+            if self.verbose :
+                print("ERROR: found no vertical segments")
 
         
     def display(self, found):
@@ -219,8 +206,6 @@ class VideoThread(threading.Thread):
 vt = VideoThread()
 
 # vt.showfound = False
-vt.showm = True
-vt.testmode = True
 
 vt.start()
 
