bf01d2efdad8dc01c10b32d10c4d9e5be7c37468
Fred Frey <f.frey6@gmail.com>
2016-02-13 01:38:04 +0000

    just changed the address

--- vision_client.py
+++ vision_client.py
@@ -8,21 +8,14 @@ s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.bind(('', 8474))
 s.settimeout(10)
 
-message = '192.168.7.1'
-
-tmp= open("/tmp/memes.dat", "w+")
+message = b'10.22.28.20'
 
 while False:
 	start_time = (time() * 1000.0)
-	s.sendto(message, ('192.168.7.2', 8474))
+	s.sendto(message, ('10.22.28.20', 8474))
 	rx = s.recv(1024)
 	
 	loop_time = (time() * 1000.0) - start_time
 	print "Received message from beaglebone, with %sms round trip." % loop_time
 	sleep(0.5)
-
-def write(text):
-   tmp.truncate()
-   tmp.write(text)
-   tmp.flush()
 
